f1a1768b14d1b2d6dd3fe4b76ca83f3621e31be6
Aaron Smith <a5mith@users.noreply.github.com>
2015-05-24 00:20:33 +0100

    Update library.js

--- library.js
+++ library.js
@@ -79,7 +79,7 @@
                         var payload = data += html;
                         } else {
                         data.postData.content += html;
-                        console.log(gfycatData);
+                        console.log(gfyItem);
                         }
                     callback(null, payload || data);
                 });
